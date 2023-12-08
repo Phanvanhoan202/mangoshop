d50c87a06437162ffb7c6d0d7e88bbe4e762d704
Phanvanhoan202 <70952493+Phanvanhoan202@users.noreply.github.com>
2023-12-08 12:44:43 +0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { githubLogo, googleLogo } from '../assets';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { facebookLogo, githubLogo, googleLogo } from '../assets';
+import { FacebookAuthProvider, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
@@ -13,6 +13,7 @@ export const Login = () => {
     const provider = new GoogleAuthProvider();
     const handleGoogleLogin = async (e) => {
         e.preventDefault();
+
         await signInWithPopup(auth, provider)
             .then((result) => {
                 const user = result.user;
@@ -33,6 +34,28 @@ export const Login = () => {
                 console.log(error);
             });
     };
+    const handleFacebookLogin = async (e) => {
+        e.preventDefault();
+
+        //cài đặt provider đăng nhập facebook
+        const facebookProvider = new FacebookAuthProvider();
+        
+        await signInWithPopup(auth, facebookProvider)
+            .then(function (result) {
+                const user = result.user;
+                dispatch(
+                    addUser({
+                        _id: user.uid,
+                        name: user.displayName,
+                        email: user.email,
+                        image: user.photoURL,
+                    }),
+                );
+            })
+            .catch(function (error) {
+                console.error('Error: hande error here>>>', error.code);
+            });
+    };
 
     const handleSignOut = () => {
         signOut(auth)
@@ -46,6 +69,21 @@ export const Login = () => {
     };
     return (
         <div className="w-full flex flex-col items-center justify-center gap-10 py-20">
+            <div className="w-full flex gap-10 items-center justify-center ">
+                <div
+                    onClick={handleFacebookLogin}
+                    className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300"
+                >
+                    <img className="w-8 " src={facebookLogo} alt="googleLogo" />
+                    <span className="text-sm text-gray-900">Sign in with Facebook</span>
+                </div>
+                <button
+                    onClick={handleSignOut}
+                    className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300"
+                >
+                    Sign out
+                </button>
+            </div>
             <div className="w-full flex gap-10 items-center justify-center ">
                 <div
                     onClick={handleGoogleLogin}
