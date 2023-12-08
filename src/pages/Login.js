dd9cb8552e76c7b1e0d466e1b4363cc7481f3c9c
Phanvanhoan202 <70952493+Phanvanhoan202@users.noreply.github.com>
2023-12-08 14:31:20 +0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,13 @@ import { facebookLogo, githubLogo, googleLogo } from '../assets';
 import { FacebookAuthProvider, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../redux/mangoSlice';
 
 export const Login = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const userInfo = useSelector((state) => state.mango.userInfo);
     const auth = getAuth();
     const provider = new GoogleAuthProvider();
     const handleGoogleLogin = async (e) => {
@@ -35,7 +36,9 @@ export const Login = () => {
     const handleFacebookLogin = async (e) => {
         //cài đặt provider đăng nhập facebook
         const facebookProvider = new FacebookAuthProvider();
-        
+        facebookProvider.setCustomParameters({
+            display: 'popup',
+        });
         await signInWithPopup(auth, facebookProvider)
             .then(function (result) {
                 const user = result.user;
@@ -65,45 +68,62 @@ export const Login = () => {
     };
     return (
         <div className="w-full flex flex-col items-center justify-center gap-10 py-20">
-            <div className="w-full flex gap-10 items-center justify-center ">
-                <div
-                    onClick={handleFacebookLogin}
-                    className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300"
-                >
-                    <img className="w-8 " src={facebookLogo} alt="googleLogo" />
-                    <span className="text-sm text-gray-900">Sign in with Facebook</span>
+            {userInfo ? (
+                <div className="flex flex-col items-center justify-center gap-10">
+                    <div>
+                        <span className="text-2xl font-medium">Xin chào:</span>
+                        <span className="text-3xl italic"> {userInfo.name}</span>
+                    </div>
+                    <button
+                        onClick={handleSignOut}
+                        className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300"
+                    >
+                        Sign out
+                    </button>
                 </div>
-                <button
-                    onClick={handleSignOut}
-                    className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300"
-                >
-                    Sign out
-                </button>
-            </div>
-            <div className="w-full flex gap-10 items-center justify-center ">
-                <div
-                    onClick={handleGoogleLogin}
-                    className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300"
-                >
-                    <img className="w-8 " src={googleLogo} alt="googleLogo" />
-                    <span className="text-sm text-gray-900">Sign in with Google</span>
+            ) : (
+                <div className="w-full flex flex-col items-center justify-center gap-10 ">
+                    <div className="w-full flex gap-10 items-center justify-center ">
+                        <div
+                            onClick={handleFacebookLogin}
+                            className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300"
+                        >
+                            <img className="w-8 " src={facebookLogo} alt="googleLogo" />
+                            <span className="text-sm text-gray-900">Sign in with Facebook</span>
+                        </div>
+                        <button
+                            onClick={handleSignOut}
+                            className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300"
+                        >
+                            Sign out
+                        </button>
+                    </div>
+                    <div className="w-full flex gap-10 items-center justify-center ">
+                        <div
+                            onClick={handleGoogleLogin}
+                            className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300"
+                        >
+                            <img className="w-8 " src={googleLogo} alt="googleLogo" />
+                            <span className="text-sm text-gray-900">Sign in with Google</span>
+                        </div>
+                        <button
+                            onClick={handleSignOut}
+                            className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300"
+                        >
+                            Sign out
+                        </button>
+                    </div>
+                    <div className="w-full flex gap-10 items-center justify-center ">
+                        <div className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300">
+                            <img className="w-8 " src={githubLogo} alt="googleLogo" />
+                            <span className="text-sm text-gray-900">Sign in with Github</span>
+                        </div>
+                        <button className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300">
+                            Sign out
+                        </button>
+                    </div>
                 </div>
-                <button
-                    onClick={handleSignOut}
-                    className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300"
-                >
-                    Sign out
-                </button>
-            </div>
-            <div className="w-full flex gap-10 items-center justify-center ">
-                <div className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300">
-                    <img className="w-8 " src={githubLogo} alt="googleLogo" />
-                    <span className="text-sm text-gray-900">Sign in with Github</span>
-                </div>
-                <button className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300">
-                    Sign out
-                </button>
-            </div>
+            )}
             <ToastContainer
                 position="top-left"
                 autoClose={2000}
