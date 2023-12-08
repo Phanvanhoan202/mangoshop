7f642beea1481242907daa3dc7980cc3e8760ff2
Phanvanhoan202 <70952493+Phanvanhoan202@users.noreply.github.com>
2023-12-08 12:46:06 +0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,6 @@ import githubLogo from './githubLogo.png';
 import googleLogo from './googleLogo.png';
 import logoLight from './logoLight.png';
 import paymentLogo from './paymentLogo.png';
+import facebookLogo from './facebookLogo.png';
 
-export { logoDark, cart, githubLogo, googleLogo, logoLight, paymentLogo };
+export { logoDark, cart, githubLogo, googleLogo, logoLight, paymentLogo, facebookLogo };
